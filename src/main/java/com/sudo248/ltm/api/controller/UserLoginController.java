738945ec19e5d446b5993f29b912550da6cbcb94
anhtcogn <anhtcogn@gmail.com>
2022-10-23 22:00:48 +0700

    [10/23/22] update login

--- src/main/java/com/sudo248/ltm/api/controller/UserLoginController.java
+++ src/main/java/com/sudo248/ltm/api/controller/UserLoginController.java
@@ -3,6 +3,7 @@ package com.sudo248.ltm.api.controller;
 import com.sudo248.ltm.api.model.Request;
 import com.sudo248.ltm.api.model.Response;
 import com.sudo248.ltm.api.model.entities.UserEntity;
+import com.sudo248.ltm.api.model.auth.Account;
 import com.sudo248.ltm.api.security.payload.LoginRequest;
 import com.sudo248.ltm.api.security.payload.Status;
 import com.sudo248.ltm.api.service.LoginService;
@@ -11,19 +12,22 @@ import com.sudo248.ltm.websocket.annotation.WsController;
 import com.sudo248.ltm.websocket.annotation.WsPost;
 import com.sudo248.ltm.websocket.controller.WebSocketController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @WsController (path = "/user/login")
-public class UserLoginController implements WebSocketController<Request<LoginRequest>, Response<String>> {
+public class UserLoginController implements WebSocketController<Request<Account>, Response<String>> {
 
     @Autowired
     LoginService loginService;
 
     @Override
-    public void onPost(Request<LoginRequest> request, Response<String> response) {
+    public void onPost(Request<Account> request, Response<String> response) {
         String message = "";
         try {
-            if (loginService.checkAccount((LoginRequest) request.getParams())) {
+            LoginRequest loginRequest = new LoginRequest(
+                    request.getPayload().getUsername(),
+                    request.getPayload().getPassword()
+            );
+            if (loginService.checkAccount(loginRequest)) {
                 message = "Đăng nhập thành công.";
             }
         }
@@ -33,34 +37,20 @@ public class UserLoginController implements WebSocketController<Request<LoginReq
         response.setPayload(message);
     }
 
-    @WsPost(path = "/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
-        String message = "";
-        try {
-            if (loginService.checkAccount(loginRequest)) {
-                message = "Đăng nhập thành công.";
-            }
-        }
-        catch (Exception e) {
-            message = loginService.checkLogin(loginRequest.getUsername());
-        }
-        return message;
-    }
-
     @Override
-    public void onGet(Request<LoginRequest> request, Response<String> response) {
+    public void onGet(Request<Account> request, Response<String> response) {
 
     }
 
 
 
     @Override
-    public void onPut(Request<LoginRequest> request, Response<String> response) {
+    public void onPut(Request<Account> request, Response<String> response) {
         WebSocketController.super.onPut(request, response);
     }
 
     @Override
-    public void onDelete(Request<LoginRequest> request, Response<String> response) {
+    public void onDelete(Request<Account> request, Response<String> response) {
         WebSocketController.super.onDelete(request, response);
     }
 }
